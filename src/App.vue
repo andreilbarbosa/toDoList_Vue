d92a10448b914c69116f276ae858ff24049baf35
andrei <andreilimabarbosa@outlook.com>
2026-03-09 20:29:21 -0300

    refactor(Objects in Tasks): Edit tasks inside of Tasks Object

--- src/App.vue
+++ src/App.vue
@@ -1,105 +1,67 @@
 <script setup>
-import { reactive } from 'vue';
+import { reactive } from "vue";
+import Head from "./components/Head.vue";
+import Form from './components/Form.vue';
+import ToDoList from "./components/ToDoList.vue";
 
-  const estado = reactive({
-    filtro: '',
-    tarefaTemp: '',
-    tarefas: [
-      {
-        titulo: 'Study TypeScript',
-        finalizada: false,
-      },
-      {
-        titulo: 'Study Vue.js',
-        finalizada: true,
-      },
-      {
-        titulo: 'Study Javascript',
-        finalizada: false,
-      }
-    ]
-  })
-
-  function completeTask(evento){
-    if(evento){
-      return true;
-    }else{
-      return false;
-    }
-  }
-
-  const getPendingTasks = () => {
-    return estado.tarefas.filter(tarefa => !tarefa.finalizada)
-  }
-
-  const getCompletedTasks = () => {
-    return estado.tarefas.filter(tarefa => tarefa.finalizada)
-  }
-
-  const getFiltredTasks = () =>{
-    const { filtro } = estado;
-
-    switch (filtro){
-      case 'pending':
-        return getPendingTasks();
-      case 'completed':
-        return getCompletedTasks();
-      default:
-        return estado.tarefas;
-    }
-  }
-
-  const taskRegister = () =>{
-    const tarefaNova = {
-      titulo: estado.tarefaTemp,
+const estado = reactive({
+  filtro: "",
+  tarefaTemp: "",
+  tarefas: [
+    {
+      titulo: "Like in my project :D!",
       finalizada: false,
-    }
+    },
+  ],
+});
 
-    estado.tarefas.push(tarefaNova);
-
-    estado.tarefaTemp = '';
+function completeTask(evento) {
+  if (evento) {
+    return true;
+  } else {
+    return false;
   }
+}
+
+const getPendingTasks = () => {
+  return estado.tarefas.filter((tarefa) => !tarefa.finalizada);
+};
+
+const getCompletedTasks = () => {
+  return estado.tarefas.filter((tarefa) => tarefa.finalizada);
+};
+
+const getFiltredTasks = () => {
+  const { filtro } = estado;
+
+  switch (filtro) {
+    case "pending":
+      return getPendingTasks();
+    case "completed":
+      return getCompletedTasks();
+    default:
+      return estado.tarefas;
+  }
+};
+
+const registerTask = () => {
+  const tarefaNova = {
+    titulo: estado.tarefaTemp,
+    finalizada: false,
+  };
+
+  estado.tarefas.push(tarefaNova);
+
+  estado.tarefaTemp = "";
+};
 </script>
+
+<!-- // -->
 
 <template>
   <div class="container">
-    <header class="p-5 m-4 mt-4 bg-light rounded-3">
-      <h1>Today's Taks</h1>
-      <p>
-        You have {{ getPendingTasks().length}} pending tasks
-      </p>
-    </header>
-    <form @submit.prevent="taskRegister">
-      <div class="row">
-        <div class="col">
-          <input :value="estado.tarefaTemp" @change="evento => estado.tarefaTemp = evento.target.value" type="text" class="form-control" placeholder="Type your task:">
-        </div>
-        <div class="col-md-2">
-          <button type="submit" class="btn btn-success">Send</button>
-        </div>
-        <div class="col-md-2">
-          <select class="form-control" @change="evento => estado.filtro = evento.target.value">
-            <option value="all">All tasks</option>
-            <option value="pending">Pending tasks</option>
-            <option value="completed">Completed tasks</option>
-          </select>
-          {{ estado.filtro }}
-        </div>
-      </div>
-    </form>
-    <ul class="list-group mt-4">
-      <li class="list-group-item" v-for="tarefa in getFiltredTasks()">
-        <input type="checkbox" @change="evento => tarefa.finalizada = evento.target.checked" :checked="tarefa.finalizada" :id="tarefa.titulo" />
-        <label class="ms-3" :class="{ done: completeTask(tarefa.finalizada)}"  :for="tarefa.titulo">
-          {{ tarefa.titulo }}
-        </label>
-      </li>
-    </ul>
+    <Head :pending-tasks="getPendingTasks().length" />
+    <Form  :register-task="registerTask" :filter-edit="evento => estado.filtro = evento.target.value" :filter="estado.filtro" :temp-task="estado.tarefaTemp" :task-edit="evento => estado.tarefaTemp = evento.target.value" />
+    <ToDoList :filtred-task="getFiltredTasks()" />
   </div>
 </template>
-
-<style scoped>
-  .done{
-    text-decoration: line-through;
-  }
-</style>
